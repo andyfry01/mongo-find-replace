00e5f03065fc952e2d9c54f814268755747b4011
Andy Fry <andyfry01@gmail.com>
2018-02-12 19:05:55 -0500

    test: add test documents
    added test document examples for use in unit test spec

---
+++ test/testDocs.js
@@ -0,0 +1,40 @@
+module.exports = {
+  withNewLines: [{
+    "first_name": "Taddeo\r\n",
+    "last_name": "MacAleese",
+    "email": "tmacaleese0@example.com\r\n",
+    "gender": "Male\r\n",
+    "ip_address": "128.34.72.237"
+  }, {
+    "first_name": "Wini\r\n",
+    "last_name": "Haslock(e)",
+    "email": "whaslocke1@stanford.edu",
+    "gender": "Female",
+    "ip_address": "117.9.94.39"
+  }, {
+    "first_name": "Clayborne\r\n",
+    "last_name": "Guillard",
+    "email": "cguillard2@blogtalkradio.com",
+    "gender": "Male",
+    "ip_address": "213.205.225.148\r\n"
+  }],
+  withoutNewLines: [{
+    "first_name": "Taddeo",
+    "last_name": "MacAleese",
+    "email": "tmacaleese0@example.com",
+    "gender": "Male",
+    "ip_address": "128.34.72.237"
+  }, {
+    "first_name": "Wini",
+    "last_name": "Haslock(e)",
+    "email": "whaslocke1@stanford.edu",
+    "gender": "Female",
+    "ip_address": "117.9.94.39"
+  }, {
+    "first_name": "Clayborne",
+    "last_name": "Guillard",
+    "email": "cguillard2@blogtalkradio.com",
+    "gender": "Male",
+    "ip_address": "213.205.225.148"
+  }]
+}
