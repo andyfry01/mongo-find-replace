ea291444c4b2456f5b48e1dd366c74073920b18e
Andy Fry <andyfry01@gmail.com>
2018-02-25 15:26:58 -0500

    refactor: add close connection method, add go method, refactor errors
    added close connection method to call in the event of an error or at the end of the program flow,
    added go method to call the individual steps of the module and update docs/save them to db,
    refactored error handling into separate class method

--- src/find-replace.js
+++ src/find-replace.js
@@ -5,17 +5,12 @@ const util = require('util')
 class MongoFindAndReplace {
   constructor(config) {
 
-    if (validate(config)) {
-      this.config = config
-      this.regex = undefined
-      this.replacement = undefined
-    }
-
-    function validate(config) {
+    let validate = (config) => {
 
       // check for no config
       if (!config) {
-        throw new Error(' Looks like you didn\'t configure the MongoFR. \n Please pass in a config object. \n See docs reference: (doc reference)')
+        let errorMessage = ' Looks like you didn\'t configure the MongoFR. \n Please pass in a config object. \n See docs reference: (doc reference)'
+        this.handleError(errorMessage)
         return false
       }
 
@@ -26,7 +21,8 @@ class MongoFindAndReplace {
       let configProperties = Object.keys(config)
       configProperties.forEach(prop => {
         if (validConfigProperties.indexOf(prop) < 0) {
-          throw new Error(`${prop} is an invalid config property`)
+          let errorMessage = `${prop} is an invalid config property`
+          this.handleError(errorMessage)
           return false
         }
       })
@@ -34,13 +30,22 @@ class MongoFindAndReplace {
       //check for missing config props
       requiredConfigProps.forEach(prop => {
         if (configProperties.indexOf(prop) < 0) {
-          throw new Error(`${prop} is a required config property`)
+          let errorMessage = `${prop} is a required config property`
+          this.handleError(errorMessage)
           return false
         }
       })
 
       // if validations pass, return true
       return true
+    }
+    
+    if (validate(config)) {
+      this.config = config
+      this.regex = undefined
+      this.replacement = undefined
+      this.connection = undefined
+      this.db = undefined
     }
   }
 
@@ -50,16 +55,29 @@ class MongoFindAndReplace {
         if (err) {
           reject(err);
         }
-        resolve(connection.db(dbName))
+        this.connection = connection
+        this.db = connection.db(dbName)
+        resolve()
       })
     })
   }
 
-  getCollection(collectionName, db) {
-    return db.collection(collectionName)
+  closeConnection() { 
+    // console.log('this.connection');
+    // console.log(this.connection);
+    if (this.connection !== undefined) {
+      this.connection.close(false, () => { 
+        this.connection = undefined
+      })
+    }
+  }
+
+  getCollection(collectionName) {
+    return this.db.collection(collectionName)
   }
 
   processDocFields(doc, regex, replacement) {
+    // console.log('am i being called?');
     let docKeys = Object.keys(doc)
     let processedDocument = docKeys.reduce((current, next) => {
       if (next === '_id') {
@@ -72,65 +90,103 @@ class MongoFindAndReplace {
     return processedDocument
   }
 
-  save(doc, collection) {
-    collection.update({
-        _id: doc._id
-      },
-      doc,
-      (err, status) => {
-        console.log(status.result);
-      })
+  save(docsBatch, collection) {
+    console.log('now saving');
+    // console.log(util.inspect(docsBatch, {showHidden: false, depth: null}))
+    collection.bulkWrite(docsBatch, (err, result) => {
+      if (err) {
+        let errorMessage = err
+        this.handleError(errorMessage)
+      } else {
+        if (result.ok === 1) {
+        } else {
+          let errorMessage = result.getWriteErrors()
+          this.handleError(errorMessage)
+        }
+      }
+      this.closeConnection()
+    })
   }
 
   go() {
-    console.log('hi!');
-
+    this.getConnection(this.config.dbUrl, this.config.dbName)
+    .then(() => {
+      this.config.collections.forEach(collectionName => {
+        let collection = this.getCollection(collectionName)
+        collection.find({}).toArray((err, docs) => {
+          if (err) {
+            console.log(err);
+            this.closeConnection()
+          }
+          let updatedDocs = docs.map(doc => {
+            let processedDoc = this.processDocFields(doc, this.regex, this.replacement)
+            let updateObject = {
+              updateOne : {
+                "filter" : { "_id" : processedDoc._id },
+                "update" : { $set : processedDoc }
+              }
+            }
+            return updateObject
+          })
+          this.save(updatedDocs, collection)
+        })
+      })
+    })
   }
 
   find(regexPattern) {
+    let validate = (input) => {
+      if (input.constructor.name !== 'RegExp') {
+        let errorMessage = 'Invalid input: #find takes a regular expression as an argument'
+        this.handleError(errorMessage)
+        return false
+      }
+      return true
+    }
+
     if (validate(regexPattern)) {
       this.regex = regexPattern
       return this
     }
+  }
 
-    function validate(input) {
-      if (input.constructor.name !== 'RegExp') {
-        throw new Error('Invalid input: #find takes a regular expression as an argument')
+  andReplaceWith(replacement) {
+    let validate = (input) => {
+      let validInputTypes = ['string', 'number', 'boolean']
+      if (validInputTypes.indexOf(typeof input) < 0) {
+        let errorMessage = 'Invalid input: #andReplaceWith takes a string, number, or boolean as input'
+        this.handleError(errorMessage)
         return false
       }
       return true
     }
-  }
 
-  andReplaceWith(replacement) {
+    let allInputsAssigned = () => {
+      if (this.replacement === undefined) {
+        let errorMessage = 'No replacement specified'
+        this.handleError(errorMessage)
+        return false
+      }
+      if (this.regex === undefined) {
+        let errorMessage = 'No regex pattern specified'
+        this.handleError(errorMessage)
+        return false
+      }
+      return true
+    }
 
     if (validate(replacement)) {
       this.replacement = replacement
-      if (allInputsAssigned(this)) {
+      if (allInputsAssigned()) {
         this.go()
       }
     }
 
-    function validate(input) {
-      let validInputTypes = ['string', 'number', 'boolean']
-      if (validInputTypes.indexOf(typeof input) < 0) {
-        throw new Error('Invalid input: #andReplaceWith takes a string, number, or boolean as input')
-        return false
-      }
-      return true
-    }
+  }
 
-    function allInputsAssigned(that) {
-      if (that.replacement === undefined) {
-        throw new Error('No replacement specified')
-        return false
-      }
-      if (that.regex === undefined) {
-        throw new Error('No regex pattern specified')
-        return false
-      }
-      return true
-    }
+  handleError(errorMessage) {
+    throw new Error(errorMessage)
+    this.closeConnection()
   }
 }
 
